09e362e8f581e6e6f3901ef9fd5557f740d68bd6
Luba Hadunova <luba.godunova2018@gmail.com>
2023-08-25 09:22:53 +0200

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
+
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        if (this.min === this.max) {
+            return this.max;
+        }
+        this.value = Math.round((this.min + this.max) / 2);
+        return this.value;
     }
 
     lower() {
-
+        this.max = this.value;
     }
 
     greater() {
-
+        this.min = this.value;
     }
 }
 
